663e95e711bd476bc821067a26f92e320a5f5fcd
Tim Scholtes <Tim.Scholtes@global.com>
2018-03-05 17:01:04 +0000

    starting generator methods for actions

--- bots.py
+++ bots.py
@@ -1,9 +1,9 @@
 import random
-
+import copy
 ## Parent bot class
 class Bot(object):
-	def __init__(self,loc,pathfinder,visibility,
-		efficiency,battery_max,storage_max):
+	def __init__(self,loc=[0,0],visibility=10,
+		efficiency=10,battery_max=10,storage_max=10,speed=10,pathfinder=None):
 
 		self.loc = loc
 		self.pathfinder = pathfinder
@@ -12,11 +12,38 @@ class Bot(object):
 		self.battery_max = battery_max
 		self.efficiency = efficiency
 		self.storage_max = storage_max
+		self.speed = speed
 		self.storage = [0]*5
 
 
+	def base_pathfinder(self,b):
+		new_loc = copy.deepcopy(self.loc)
+		while new_loc != b:
+			if new_loc[0] != b[0]:
+				if new_loc[0] < b[0]:
+					new_loc[0] += 1
+				else:
+					new_loc[0] -= 1
+			elif new_loc[1] != b[1]:
+				if new_loc[1] < b[1]:
+					new_loc[1] += 1
+				else:
+					new_loc[1] -= 1
+			yield (new_loc,self.speed)
+		return StopIteration
+		
+
+
+
+
+
+	
+	def move(self,new_loc):
+		self.loc = new_loc
+
+
 class Miner(Bot):
-	def __init__(self,speed_boost,drill_depth):
+	def __init__(self,speed_boost=5,drill_depth=5):
 		super(Miner, self).__init__()
 		self.speed = random.randint(2,5) + speed_boost
 		self.drill_depth = drill_depth
--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import maps
 import bots
-
+import collections
 
 class Game:
 
@@ -9,7 +9,7 @@ class Game:
 		self.bots = {}
 		self.buildings = {}
 		self.maps = maps.Maps(N,depth)
-		self.schedule = []
+		self.schedule = collections.defaultdict(collections.deque)
 		self.score = 0
 		self.resources = [100]*5
 		self.year = 2025.0
@@ -17,25 +17,51 @@ class Game:
 		self.t_per_season = 100
 
 		
-	def add_actions(self,action_lst):
-		self.schedule.append(action_lst)
+	def add_action(self,bot_id,action):
+		self.schedule[bot_id].appendleft(action)
 
 	def execute_turn(self,verbose=0):
-		pass
+		"""
+		Scheduling is important! Trying to avoid race conditions.
+		Each element in the schedule is a generator (a super-task)
+		this super-task creates sub tasks that get assigned onto a heap
+		the heap is indexed by time that it can be completed, as I want to include
+		delays to make some actions take longer.
 
-	def move_bot(self,bot_id,new_loc):
-		this_bot = self.bots[bot_id[0]][bot_id[1]].loc
-		this_bot.loc = new_loc
-		self.uncover(new_loc[0],new_loc[1],this_bot.visibility)
+		The control flow is to :
+		1. take the intial actions from all active super-tasks
+		2. heapify, with time schedule as the min-key
+		3. while heap and t < 100:
+			a. extract-min from the heap.
+			b. do the task
+				i. this task may end with adding a new task to the heap.
+					eg. move a-b may end with req to move b-c next.
+				ii. if the super task has ended, then pop it and 
+					enqueue the new generator, get next from this
+			c. t += 1
+		"""
 
 	def uncover(self,i,j,vis_range):
 		self.maps.mask[i-vis_range:i+vis_range,
 		j-vis_range:j+vis_range] = 1
 
 
+if __name__ == '__main__':
+	game = Game(20,3)
+	game.bots = {
+	'm1':bots.Miner(),
+	'm2':bots.Miner(),
+	'm3':bots.Miner()}
 
 
 
+	game.add_action('m1',game.bots['m1'].base_pathfinder([10,10]))
+	game.add_action('m2',game.bots['m2'].base_pathfinder([-10,-10]))
+	print(game.schedule['m1'])
+	print(game.schedule['m3'])
+
+	for k in range(30):
+		print(next(game.schedule['m1'][len(game.schedule['m1'])-1]))
 
 
 
